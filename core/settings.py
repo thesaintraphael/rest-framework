2be1a14d32145c6c54ac43136073e4b3d2de49c8
Rafael Salimov <raphasalimov@gmail.com>
2021-11-03 21:54:06 +0400

    used python-decouple to manage .env variables

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,8 +129,8 @@ STATIC_URL = '/static/'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = os.environ.get("EMAIL")
-EMAIL_HOST_PASSWORD = os.environ.get("PASSWORD")
+EMAIL_HOST_USER = config("EMAIL")
+EMAIL_HOST_PASSWORD = config("PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = os.environ.get("EMAIL")
+DEFAULT_FROM_EMAIL = config("EMAIL")
