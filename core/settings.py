feb378bdefd643c5a2d33568ca92d61e38d59918
Rafael Salimov <raphasalimov@gmail.com>
2021-11-29 20:35:52 +0400

    cors headers

--- core/settings.py
+++ core/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
-    'corsheaders'
+    'corsheaders',
 
     # Local aps
     'accounts.apps.AccountsConfig',
