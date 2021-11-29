0bfd83cebd682fac7285692bcaec0412f36a7c11
Rafael Salimov <raphasalimov@gmail.com>
2021-11-29 21:45:29 +0400

    api doc json format

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -28,7 +28,9 @@ urlpatterns = [
 
     # Swagger
     path('swagger/', schema_view.with_ui('swagger',
+                                         cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/api.json', schema_view.without_ui(
         cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
-        cache_timeout=0), name='schema-redoc'),
+                                       cache_timeout=0), name='schema-redoc'),
 ]
