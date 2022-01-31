43c6551f6c42ea87ec876302aa7bc0fcbbdff907
Rafael Salimov <raphasalimov@gmail.com>
2022-01-31 23:55:02 +0400

    mocking send_email function

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from .test_setup import TestSetup
 from ..models import User
 
@@ -8,12 +10,14 @@ class TestView(TestSetup):
         response = self.client.post(self.register_url)
         self.assertEqual(response.status_code, 400)
 
-    def test_user_can_register_correctly(self):
+    @patch("accounts.views.send_email")
+    def test_user_can_register_correctly(self, mock_email):
         response = self.client.post(
             self.register_url, self.user_data, format='json'
         )
 
         self.assertEqual(response.status_code, 200)
+        mock_email.assert_called_once()
 
     def test_user_cannot_login_with_unverified_email(self):
         self.client.post(
@@ -22,12 +26,18 @@ class TestView(TestSetup):
             self.login_url, self.user_data, format="json")
         self.assertEqual(response.status_code, 401)
 
-    def test_user_can_login_after_verification(self):
+    @patch("accounts.views.send_email")
+    def test_user_can_login_after_verification(self, mock_email):
         self.client.post(
             self.register_url, self.user_data, format="json")
+        
         email = self.user_data.get('email')
         user = User.objects.get(email=email)
         user.is_verified = True
         user.save()
+        
         res = self.client.post(self.login_url, self.user_data, format="json")
         self.assertEqual(res.status_code, 200)
+
+        mock_email.assert_called_with(
+            self.user_data['email'], user.activation_code)
