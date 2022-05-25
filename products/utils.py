87018d663106ce9164b5d180e8988cc7c6d0a5c3
Rafael Salimov <raphasalimov@gmail.com>
2022-05-25 12:08:26 +0400

    added new util functions to SerializerUtil class

--- products/utils.py
+++ products/utils.py
@@ -6,6 +6,22 @@ class SerializerUtil:
     def __init__(self, serializer_class) -> None:
         self.serializer_class = serializer_class
 
+    @staticmethod
+    def get_request_from_context(context):
+        return context.get('request')
+
+    @staticmethod
+    def required(value, field: str = None) -> None:
+        """field is string representation of field name. Used to return error_message with key value"""
+
+        error_message = "This field is required"
+
+        if not value:
+            if field:
+                error_message = {f"{field}": error_message}
+
+            raise serializers.ValidationError(error_message)
+
     def save_serializer(self, data, instance=None, context=None, **kwargs):
 
         serializer = self.serializer_class(
@@ -21,7 +37,8 @@ def validate_materials(data, materials):
     add_materials = data.pop('add_materials')
 
     if add_materials:
-        serializer = serializers.MaterialSerializer(data=materials, many=True)
+        serializer = serializers.MaterialSerializer(
+            data=materials, many=True)
         serializer.is_valid(raise_exception=True)
         return serializer.save()
 
